9e9a4082176e3c773ac8da489dcbeb5751c50830
zhongli20061006 <2795524983@qq.com>
2026-08-05 14:54:58 +0800

    feat(miniprogram): 下单成功页

--- miniprogram/pages/order-success/order-success.js
+++ miniprogram/pages/order-success/order-success.js
@@ -1,1 +1,24 @@
-Page({})
+Page({
+  data: {
+    orderNo: '',
+    totalCents: 0,
+    storeName: '',
+  },
+
+  onLoad(options) {
+    this.setData({
+      orderNo: options.order_no || '',
+      totalCents: Number(options.total_cents || 0),
+      storeName: decodeURIComponent(options.store_name || ''),
+    })
+  },
+
+  viewOrder() {
+    // pending_order_display 已由 checkout 在提交成功后写入，onShow 直接渲染
+    wx.switchTab({ url: '/pages/my-orders/my-orders' })
+  },
+
+  goHome() {
+    wx.switchTab({ url: '/pages/store-list/store-list' })
+  },
+})
